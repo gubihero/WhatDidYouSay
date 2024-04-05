1372bc467194c92e58855ac4e5365e00828b7469
James Gubler <gubimail@gmail.com>
2024-04-04 21:36:15 -0600

    adds drawpad functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DrawpadComponent } from './components/drawpad/drawpad.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DrawpadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/drawpad/drawpad.component.ts
+++ src/app/components/drawpad/drawpad.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { fromEvent, pairwise, switchMap, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-drawpad',
@@ -7,6 +8,88 @@ import { Component } from '@angular/core';
   templateUrl: './drawpad.component.html',
   styleUrl: './drawpad.component.scss'
 })
-export class DrawpadComponent {
+export class DrawpadComponent implements AfterViewInit {
+  @ViewChild('drawpad') public canvas: ElementRef | undefined;
 
+  @Input() height: number = 400;
+  @Input() width: number = 400;
+
+  
+  private cx: CanvasRenderingContext2D | null | undefined;
+  
+  ngAfterViewInit(): void {
+    const canvasEl: HTMLCanvasElement = this.canvas?.nativeElement;
+    console.log('afterviewinit')
+    this.cx = canvasEl.getContext('2d');
+
+    canvasEl.width = this.width;
+    canvasEl.height = this.height;
+
+    if (!this.cx) throw 'Cannot get context';
+
+    this.cx.lineWidth = 3;
+    this.cx.lineCap = 'round';
+    this.cx.strokeStyle = '#000';
+
+    this.captureEvents(canvasEl);
+  }
+
+  private captureEvents(canvasEl: HTMLCanvasElement) {
+    // this will capture all mousedown events from the canvas element
+    fromEvent(canvasEl, 'mousedown')
+      .pipe(
+        switchMap(e => {
+          // after a mouse down, we'll record all mouse moves
+          console.log('switchmap')
+          return fromEvent(canvasEl, 'mousemove').pipe(
+            // we'll stop (and unsubscribe) once the user releases the mouse
+            // this will trigger a 'mouseup' event
+            takeUntil(fromEvent(canvasEl, 'mouseup')),
+            // we'll also stop (and unsubscribe) once the mouse leaves the canvas (mouseleave event)
+            takeUntil(fromEvent(canvasEl, 'mouseleave')),
+            // pairwise lets us get the previous value to draw a line from
+            // the previous point to the current point
+            pairwise()
+          );
+        })
+      )
+      .subscribe((res) => {
+        console.log('res ', res)
+        const rect = canvasEl.getBoundingClientRect();
+        const prevMouseEvent = res[0] as MouseEvent;
+        const currMouseEvent = res[1] as MouseEvent;
+
+        // previous and current position with the offset
+        const prevPos = {
+          x: prevMouseEvent.clientX - rect.left,
+          y: prevMouseEvent.clientY - rect.top
+        };
+
+        const currentPos = {
+          x: currMouseEvent.clientX - rect.left,
+          y: currMouseEvent.clientY - rect.top
+        };
+        console.log('currentpos ', currentPos)
+        // this method we'll implement soon to do the actual drawing
+        this.drawOnCanvas(prevPos, currentPos);
+      });
+  }
+
+  private drawOnCanvas(
+    prevPos: { x: number; y: number },
+    currentPos: { x: number; y: number }
+  ) {
+    console.log('drawoncanvase')
+    if (!this.cx) {
+      return;
+    }
+
+    this.cx.beginPath();
+
+    if (prevPos) {
+      this.cx.moveTo(prevPos.x, prevPos.y); // from
+      this.cx.lineTo(currentPos.x, currentPos.y);
+      this.cx.stroke();
+    }
+  }
 }
