ed56da1635acc0865b65708adad602dbeb45bfa2
James Gubler <gubimail@gmail.com>
2024-04-04 21:50:21 -0600

    initial layout in game for drawpad and picture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DrawpadComponent } from './components/drawpad/drawpad.component';
+import { GameComponent } from './components/game/game.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DrawpadComponent],
+  imports: [RouterOutlet, GameComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { DrawpadComponent } from '../drawpad/drawpad.component';
+import { PictureComponent } from '../picture/picture.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [DrawpadComponent,PictureComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
